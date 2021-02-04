57ca112c7fdcda379d5e560052eb419a0d3a735c
sangseunglee <lss9209>
2021-02-04 19:48:40 +0900

    Project of QueryDSL Day 1

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,20 @@
+package study.querydsl.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+
+}
