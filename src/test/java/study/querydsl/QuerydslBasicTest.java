c1a18975619290f7ddcbd1ce566b5db98cd71526
sangseunglee <lss9209>
2021-02-05 20:00:28 +0900

    Project of QueryDSL Day 2

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,17 @@
+package study.querydsl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+
+}
